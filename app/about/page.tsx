934146144549b862a8670b32121d0e0df5407c32
Kelsi Strange <kelsi@webdesignpros365.com>
2026-09-11 17:03:56 +0000

    About team cards: object-center on photos so faces stay centered on mobile (no beheading)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -146,7 +146,7 @@ export default function AboutPage() {
                   <img
                     src={member.photo}
                     alt={member.name}
-                    className="w-full h-full object-cover object-top"
+                    className="w-full h-full object-cover object-center"
                   />
                 </div>
                 <div className="p-6">
